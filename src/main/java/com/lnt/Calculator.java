d33cd552b54feb3b521701c939f8dd895df5896c
albin-trainer <69448620+albin-trainer@users.noreply.github.com>
2022-03-03 14:50:22 +0530

    Update Calculator.java

--- src/main/java/com/lnt/Calculator.java
+++ src/main/java/com/lnt/Calculator.java
@@ -2,7 +2,8 @@ package com.lnt;
 
 public class Calculator {
  int add(int a,int b) {
-	 Sstem.out.println("Doing Addition");
+	 int a=10;
+	 System.out.println("Doing Addition");
 	 return a+b;
  }
 }
